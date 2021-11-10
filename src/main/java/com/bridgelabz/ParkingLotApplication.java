2b521031232a5399b1b129a3a76235fd1c35dc5c
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-11-10 15:27:18 +0530

    Added: Test Case and Method For Vehicle Park

--- src/main/java/com/bridgelabz/ParkingLotApplication.java
+++ src/main/java/com/bridgelabz/ParkingLotApplication.java
@@ -4,9 +4,13 @@ package com.bridgelabz;
  * Purpose To Simulate With Parking Lot Problem
  *
  * @author KUNAL SURYAWANSHI
+ * @since 10/11/2021
  */
 
 public class ParkingLotApplication {
+
+    private Object Vehicle;
+
     /**
      * Purpose To Print Given Welcome Message
      *
@@ -15,5 +19,15 @@ public class ParkingLotApplication {
      */
     public String welcomeMessage(String message) {
         return message;
+    }
+
+    /**
+     * Purpose To Park Given Vehicle
+     *
+     * @param vehicle given vehicle as parameter
+     * @return True For Vehicle Parked
+     */
+    public boolean Park(Object vehicle) {
+        return true;
     }
 }
--- src/test/java/com/bridgelabz/ParkingLotApplicationApplicationTest.java
+++ src/test/java/com/bridgelabz/ParkingLotApplicationApplicationTest.java
@@ -1,14 +1,27 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ParkingLotApplicationApplicationTest {
+    ParkingLotApplication service;
+
+    @BeforeEach
+    void setUp() {
+        service = new ParkingLotApplication();
+    }
+
     @Test
-    public void givenWelcomeMessage_ShouldReturn() {
+    public void givenWelcomeMessage_ShouldReturnEqual() {
         String message = "Welcome To The Parking Lot Problem.!";
-        ParkingLotApplication service = new ParkingLotApplication();
         String messageCheck = service.welcomeMessage(message);
         Assertions.assertEquals(message, messageCheck);
+    }
+
+    @Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+        boolean isParked = service.Park(new Object());
+        Assertions.assertTrue(isParked);
     }
 }
